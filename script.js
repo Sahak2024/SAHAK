c385e15597b13ff3ff03d5ae4dec6cb7631b772e
SAHAK <sahak06072003@gmail.com>
2025-10-19 11:10:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let linklist = document.getElementById("link_list");
+
+
+linklist.style.maxHeight ="0px";
+
+function togglemenu(){
+ if( linklist.style.maxHeight =="0px"){
+   linklist.style.maxHeight ="300px";
+ }
+ else{
+   linklist.style.maxHeight ="0px";
+ }
+}
+
+
